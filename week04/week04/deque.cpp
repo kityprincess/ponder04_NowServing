73d13b875934621a5ca696aae8b0265d55054348
Matthew Burr <mdburr@outlook.com>
2017-05-06 10:16:49 -0500

    Added comments to the base implementation file.

--- week04/week04/deque.cpp
+++ week04/week04/deque.cpp
@@ -1,3 +1,12 @@
+/***********************************************************************
+* Implementation:
+*    DEQUE
+* Summary:
+*    This implements the deque class
+* Author
+*    Matthew Burr
+**********************************************************************/
+
 #include "deque.h"
 
 
